b50af1acd31c2192a64eae8967b638376291db6d
Zeitoros <aituimash@gmail.com>
2026-01-29 21:10:05 +0500

    operators

---
+++ practice_2/w3schools_2/boolean/boolean_operators.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+strict_1 = a > b # a strictly greater than b
+strict_2 = a < b # a strictly less than b
+
+more_or_equal = a >= b # a more or equal than b
+less_or_equal = a <= b # a less or equal than b
+
+equivalent = a == b # a equals b
