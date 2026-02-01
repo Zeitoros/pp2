3b3d447d95aa013e755bc44797c36477feaa68f4
Zeitoros <aituimash@gmail.com>
2026-02-01 20:32:07 +0500

    switch

---
+++ practice_2/w3schools_2/switch/switch.py
@@ -0,0 +1,17 @@
+day = int(input())
+
+match day:
+    case 1:
+        print("Mon")
+    case 2:
+        print("Tue")
+    case 3:
+        print("Wed")
+    case 4:
+        print("Thur")
+    case 5:
+        print("Fri")
+    case 6:
+        print("Sat")
+    case 7:
+        print("Sun")
