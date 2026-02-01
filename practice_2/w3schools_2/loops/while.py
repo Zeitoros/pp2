9559be6e095be8ed0b9c1e57825a067322f22f9e
Zeitoros <aituimash@gmail.com>
2026-02-01 20:20:15 +0500

    while loops

--- practice_2/w3schools_2/loops/while.py
+++ practice_2/w3schools_2/loops/while.py
@@ -1,3 +1,13 @@
+n = int(input())
+i = 0
+
+while (i <= n):
+    if (i % 2 == 0 and i != 0):  # if i is even and not equals 0
+        print(i, end=" ") # print i which is even (exclude 0)
+    i += 1 # incrementation
+
+print(" ") # separator between two loops
+
 i = 1
 while i < 6:
     print(i, end=" ")
