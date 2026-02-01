61b5a80e5ebe1c707113884d0d47be388375bc62
Zeitoros <aituimash@gmail.com>
2026-02-01 22:00:56 +0500

    for ex_2(upd)

--- practice_2/w3schools_2/loops/for_ex-2.py
+++ practice_2/w3schools_2/loops/for_ex-2.py
@@ -1,2 +1,19 @@
 for i in range(5):
-    print(f"Шаг #{i}")
+    print(f"Step #{i}")
+
+
+for i in range(1, 6):
+    if i % 2 == 0:
+        continue  # skip even numbers
+    print(f"Odd number: {i}")
+
+
+items = ["coin", "enemy", "health_pack"]
+
+for item in items:
+    if item == "coin":
+        print("Add 10 pts")
+    elif item == "enemy":
+        pass  # TODO: add a logic for getting damage later
+    elif item == "health_pack":
+        print("Healing...")
